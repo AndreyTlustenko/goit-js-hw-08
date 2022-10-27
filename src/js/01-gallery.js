8696b66097793ba9d6c785106faccb7e32f8e02f
AndreyTlustenko <andreyt2018@ukr.net>
2022-10-27 20:33:34 +0300

    upd2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,50 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('div.gallery');
+const cards = createGalleryPictures(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cards);
+
+function createGalleryPictures(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+    return `
+    <a class="gallery__item"  href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>`
+}).join('')
+};
+console.log(galleryItems);
+
+// galleryContainer.addEventListener(`click`, toClickGallery)
+
+// function toClickGallery(e){
+//     // e.preventDefault();
+//     if (!e.target.classList.contains('gallery__image')) {
+//         return
+//     }
+// };
+ const lightbox = new SimpleLightbox('.gallery a', {captionsData:"alt", captionDelay: 250});
+
+
+// const galleryContainer = document.querySelector('.gallery');
+// const cards = createGalleryPictures(galleryItems);
+
+// galleryContainer.insertAdjacentHTML('beforeend', cards);
+
+// function createGalleryPictures(galleryItems) {
+//     return galleryItems.map(({preview, original, description}) => {
+//     return `
+//     <a class="gallery__item"  href="${original}">
+//     <img class="gallery__image" src="${preview}" alt="${description}"/>
+//     </a>`
+// }).join('')
+// };
+
+// let lightbox = new SimpleLightbox('.gallery a', {captionsData:"alt", captionDelay: 250});
+
+
