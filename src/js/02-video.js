2644c49ae4035c2f3de2e10ffe891263bdab3da5
AndreyTlustenko <andreyt2018@ukr.net>
2022-10-29 13:56:22 +0300

    up

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,22 +2,24 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 let Iframe = document.querySelector('iframe');
-    let player = new Player(Iframe);
+    const player = new Player(Iframe);
 
-// player.on('play', function() {
-//         console.log('played the video!');
-//     });
-// player.getVideoTitle().then(function(title) {
-//         console.log('title:', title);
-//     });
-   
-onPlay=throttle((data)=>{
-    localStorage.setItem("videoplayer-current-time", data.seconds);
-}, 1000);
-player.on("timeupdate",  onPlay);
+player.on('play', function() {
+        console.log('played the video!');
+    });
+player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
 
 player.on("loaded", function () {
     if (localStorage.getItem("videoplayer-current-time")) {
         player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
     }
 });
+
+const onPlay = throttle((data)=>{
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}, 1000);
+
+player.on("timeupdate",  onPlay);
+
